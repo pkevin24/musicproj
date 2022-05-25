b77a0701089a1c3fbfc1b38a1efa24eae1a69914
pkevin24 <70275725+pkevin24@users.noreply.github.com>
2022-05-25 12:05:20 +0530

    .

--- src/app/fav/fav.component.ts
+++ src/app/fav/fav.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { DatasService } from '../datas.service';
 
 @Component({
   selector: 'app-fav',
   templateUrl: './fav.component.html',
-  styleUrls: ['./fav.component.css']
+  styleUrls: ['./fav.component.css'],
+  providers:[DatasService]
 })
 export class FavComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private dataService:DatasService) { }
+  datas:any=[];
+  list:any=[];
   ngOnInit(): void {
+    this.dataService.getAllDetails().subscribe((allData)=>{
+      console.log(allData);
+      this.datas=allData;
+      this.list = this.datas.filter((item:any) => item.fav == true);
+      console.log(this.list);
+      console.log(this.datas)
+      
+    })
+    
   }
 
 }
