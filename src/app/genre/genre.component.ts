b7875360dd994827f9b91d379eacbe4a821f7194
pkevin24 <70275725+pkevin24@users.noreply.github.com>
2022-05-26 23:18:27 +0530

    .

--- src/app/genre/genre.component.ts
+++ src/app/genre/genre.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { DatasService } from '../datas.service';
 
 @Component({
   selector: 'app-genre',
   templateUrl: './genre.component.html',
-  styleUrls: ['./genre.component.css']
+  styleUrls: ['./genre.component.css'],
+  providers:[DatasService]
 })
 export class GenreComponent implements OnInit {
-
-  constructor() { }
-
+  genres:any=[{names:'Rock'},{names:'Classic'},{names:'Hiphop'}];
+  addGenre=new FormGroup({
+    category:new FormControl('')
+  })
+  genre:any=[];
+  constructor(private fb:FormBuilder,private dataService:DatasService) { }
+  datas:any=[];
+  list:any=[];
   ngOnInit(): void {
+    this.sendGenre();
+    
+}
+  SaveData(){
+    console.log(this.addGenre)
+    console.log(this.addGenre.value);
+    this.genre=this.addGenre.value;
+    console.log(this.genre.category);
+    this.dataService.getAllDetails().subscribe((allData)=>{
+      console.log(allData);
+      this.datas=allData;
+      this.list = this.datas.filter((item:any) => item.genre==this.genre.category);
+      console.log(this.list);
+      console.log(this.datas)
+  })
+  }
+  sendGenre(){
+    console.log(this.genre);
   }
 
 }
